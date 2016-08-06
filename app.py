21e873ad72c88232d63dfdbdbd77f06103e94a99
baojiwei <baojiwei@baojiwei.com>
2016-08-07 04:03:24 +0800

    get image and push image into reds

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 from camera import Camera
-from redis
+import redis
+from cStringIO import StringIO
+from datetime import datetime
 
 app = Flask(__name__)
+
+cache_db = redis.StrictRedis()
 
 @app.route('/')
 def index():
@@ -22,9 +26,29 @@ def video_feed():
     return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/video_push')
+@app.route('/video_push', methods=['POST'])
 def video_push_cache():
     '''推流'''
+    file = request.files('uploaded_file')
+    timeout = 10
+    content = StringIO(file.read())
+
+    #先把图像数据放入到setex中
+        #先以时间戳生成key
+    curimage_key = str(datetime.now)
+    cache_db.setex(curimage_key,content,timeout)
+
+    #生成序列组
+    image_indexs = range(1,11)
+    image_keys = cache_db.hmget('images',image_indexs)
+    image_keys = image_keys[1:]
+    image_keys.append(curimage_key)
+
+    #更新
+    vals = {}
+    for i in image_indexs:
+        vals.update({i:image_keys[i]})
+    cache_db.hmset('images',vals)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, threader=True)
--- camera.py
+++ camera.py
@@ -2,10 +2,12 @@
 # encoding: utf-8
 
 from time import time
+import redis
 
+cache_db = redis.StrictRedis()
 class Camera(object):
     def __init__(self):
-        self.frames = [open(f + '.jpg', 'rb').read() for f in ['1', '2', '3']]
+        pass
 
     def get_frame(self):
         return self.frames[int(time()*2) %3 ]
