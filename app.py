61b8859664b66b3ce8f632fc9a8a4032dc2c2abd
baojiwei <baojiwei@baojiwei.com>
2016-08-28 00:31:27 +0800

    update not test

--- app.py
+++ app.py
@@ -1,55 +1,52 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from flask import Flask, render_template, Response, request
+from flask import Flask, render_template, Response, request, session
 from camera import Camera
 import redis
+import os
 from cStringIO import StringIO
-from datetime import datetime
 
 app = Flask(__name__)
 
 cache_db = redis.StrictRedis()
+camera = Camera()
+local_access = {}
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-def gen(camera):
-    flag = True
-    while flag:
-        frame,flag = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+def gen(listener):
+    #flag = True
+    #while flag:
+    #    frame,flag = camera.get_frame(acess_key)
+    yield (b'--frame\r\n'
+           b'Content-Type: image/jpeg\r\n\r\n' + listener.next() + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(Camera()),
+    if not session['session_id']:
+        access_key = session['session_id'] = os.urandom(24)
+    else:
+        access_key = session['session_id']
+
+    if local_access.has_key(access_key):
+        local_access[access_key].close()
+    local_access[access_key] = cache_db.pubsub()
+    local_access[access_key].subscribe(camera.name)
+
+    return Response(gen(local_access[access_key]),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/video_push', methods=['POST'])
 def video_push_cache():
     '''推流'''
     file = request.files('uploaded_file')
-    timeout = 10
     content = StringIO(file.read())
 
-    #先把图像数据放入到setex中
-        #先以时间戳生成key
-    curimage_key = str(datetime.now)
-    cache_db.setex(curimage_key,content,timeout)
-
-    #生成序列组
-    image_indexs = range(0,10)
-    image_keys = cache_db.hmget('images',image_indexs)
-    image_keys = image_keys[1:]
-    image_keys.append(curimage_key)
-
-    #更新
-    vals = {}
-    for i in image_indexs:
-        vals.update({i:image_keys[i]})
-    cache_db.hmset('images',vals)
+    curimage_key = camera.save_frame(content)
+    cache_db.publish(camera.name, curimage_key)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, threader=True)
--- camera.py
+++ camera.py
@@ -3,11 +3,33 @@
 
 from time import time
 import redis
+from datetime import datetime
 
 cache_db = redis.StrictRedis()
 class Camera(object):
-    def __init__(self):
-        pass
+    def __init__(self,name='default_camera'):
+        self.name = name
 
     def get_frame(self):
         return self.frames[int(time()*2) %3 ]
+
+    def save_frame(self,content,numbers=10):
+        #key:value储存时间为hmtable中的长度的100倍
+        timeout = numbers*100
+        curimage_key = str(datetime.now)
+        cache_db.setex(curimage_key,content,timeout)
+
+        #生成序列组
+        #每次丢掉第一帧
+        image_indexs = range(0,numbers)
+        image_keys = cache_db.hmget(self.name,image_indexs)
+        image_keys = image_keys[1:]
+        image_keys.append(curimage_key)
+
+        #更新
+        vals = {}
+        for i in image_indexs:
+            vals.update({i:image_keys[i]})
+        cache_db.hmset(self.name,vals)
+        #返回当前的key
+        return curimage_key
